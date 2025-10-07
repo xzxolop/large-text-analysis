1e1f60b32f6ca8955ba5d3c0ae8d0f58ab19cd05
Voronin Egor <evoroni2004@gmail.com>
2025-10-07 22:45:24 +0300

    chore: Добавил токенизацию текста и предложений

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import kagglehub
 import pandas as pd
+import nltk
 
 # Download latest version
 path = kagglehub.dataset_download("pavellexyr/the-reddit-dataset-dataset")
@@ -8,7 +9,14 @@ print("Path to dataset files:", path)
 posts_df = pd.read_csv(path + "\\the-reddit-dataset-dataset-posts.csv")
 comments_df = pd.read_csv(path + "\\the-reddit-dataset-dataset-comments.csv")
 
-print(posts_df)
-print(comments_df)
+#print(posts_df)
+#print(comments_df)
 
+comments_body = comments_df["body"]
+s1 = comments_body[0]
+print(s1)
+sentances = nltk.sent_tokenize(s1)
+print(sentances)
 
+tokens = nltk.word_tokenize(s1)
+print(tokens)

31c56e6e2a95d60f39e85ace12e61f05c113aca8
Voronin Egor <evoroni2004@gmail.com>
2025-10-10 14:20:41 +0300

    feat: tf-idf for all words

--- main.py
+++ main.py
@@ -1,31 +1,59 @@
 import kagglehub
 import pandas as pd
-import nltk
 from pathlib import Path
 
 import core
-from utils import count_word_matches
-
-# Note: Необходимо выполнить при первом запуске
-nltk.download('punkt_tab')
 
 path = Path(kagglehub.dataset_download("pavellexyr/the-reddit-dataset-dataset"))
-path_to_posts = path.joinpath("the-reddit-dataset-dataset-posts.csv")
 path_to_comments = path.joinpath("the-reddit-dataset-dataset-comments.csv")
 
-posts_df = pd.read_csv(path_to_posts)
 comments_df = pd.read_csv(path_to_comments)
 comments_body = comments_df["body"]
 
-sentences = core.tokenize_sentances(comments_body)
-words = core.tokenize_words(sentences)
+df = pd.DataFrame()
+df['document'] = comments_body
+print(df)
 
-tf = core.calculate_TF("of", sentences[0])
-print("tf:", tf )
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
-idf = core.claculate_IDF("of", sentences)
-print("idf:", idf)
+nltk.download('punkt')
+nltk.download('stopwords')
 
-search_word = "data"
-cnt_words = count_word_matches(search_word, words)
-print(f"Колличество слов {search_word}: {cnt_words}")
+stop_words = set(stopwords.words('russian'))
+
+def preprocess(text):
+    if not isinstance(text, str):
+        return ''
+
+    tokens = word_tokenize(text.lower())
+    filtered_tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
+    return ' '.join(filtered_tokens)
+
+df['processed'] = df['document'].apply(preprocess)
+print(df)
+
+# Применение tf-idf
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(df['processed'])
+
+# Вывод TF-IDF в виде массивов
+print(tfidf_matrix.toarray())
+print(vectorizer.get_feature_names_out())
+
+# Вывод в виде таблицы
+tfidf_df = pd.DataFrame(tfidf_matrix.toarray(), columns=vectorizer.get_feature_names_out())
+print(tfidf_df)
+
+cnt = 0
+# Вывод в виде каждого документа отдельно
+# TODO: сделать вывод наиболее важных if value > 0
+for index, row in tfidf_df.iterrows():
+    print(f"Документ {index + 1}:")
+    print(row.sort_values(ascending=False).head(5)) # выводит самые важные, но значение константно
+    cnt += 1
+    if (cnt == 100):
+        break

ecf8677d6b9c300668242764dc3e7dfc82de1ae0
Voronin Egor <evoroni2004@gmail.com>
2025-10-18 15:07:51 +0300

    refactor: move functions to core

--- core.py
+++ core.py
@@ -1,6 +1,13 @@
 import nltk
-import utils
 import math
+import kagglehub
+from pathlib import Path
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import pandas as pd
+import streamlit as st
+
+import utils
 
 def tokenize_sentances(text):
     sentences = []
@@ -53,3 +60,63 @@ def print_searched_words(words, count):
             break
         print(f"{key}: {value}")
         count -= 1    
+
+
+@st.cache_data
+def load_data():
+    print("start load")
+    path = Path(kagglehub.dataset_download("pavellexyr/the-reddit-dataset-dataset"))
+    path_to_comments = path.joinpath("the-reddit-dataset-dataset-comments.csv")
+
+    comments_df = pd.read_csv(path_to_comments)
+    comments_body = comments_df["body"]
+
+    df = pd.DataFrame()
+    df['document'] = comments_body
+
+    nltk.download('punkt')
+    nltk.download('stopwords')
+
+    stop_words = set(stopwords.words('english'))
+
+    def preprocess(text):
+        if not isinstance(text, str):
+            return ''
+
+        tokens = word_tokenize(text.lower())
+        filtered_tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
+        return ' '.join(filtered_tokens)
+
+    df['processed'] = df['document'].apply(preprocess)
+    print("end load")
+    return df
+
+def search_word_func():
+    word = st.session_state['text_input']
+    df = st.session_state['text_df']
+    m = make_map_most_popular(word, df['processed'])
+
+    sorted_items = sorted(m.items(), key=lambda x: x[1], reverse=True)    
+
+    print_searched_words(sorted_items, 10)
+
+    def create_list(words, size: int):
+        word_list = []
+        count_list = []
+        for i in range(0, size+1):
+            word_list.append(words[i][0])
+            count_list.append(words[i][1])
+        d = {'word':word_list, 'count':count_list}           
+        df = pd.DataFrame(data=d)
+        return df
+    
+    df = create_list(sorted_items, 10)
+    print(df)
+
+    if 'data_frame' in st.session_state:
+        print('is yet!')
+        print(st.session_state['data_frame'])
+    else:
+        print('not yet!')
+
+    st.session_state['data_frame'] = df 
--- main.py
+++ main.py
@@ -1,74 +1,10 @@
-import kagglehub
+
 import pandas as pd
-from pathlib import Path
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 import streamlit as st
 
 import core
 
-@st.cache_data
-def load_data():
-    print("start load")
-    path = Path(kagglehub.dataset_download("pavellexyr/the-reddit-dataset-dataset"))
-    path_to_comments = path.joinpath("the-reddit-dataset-dataset-comments.csv")
-
-    comments_df = pd.read_csv(path_to_comments)
-    comments_body = comments_df["body"]
-
-    df = pd.DataFrame()
-    df['document'] = comments_body
-
-    nltk.download('punkt')
-    nltk.download('stopwords')
-
-    stop_words = set(stopwords.words('english'))
-
-    def preprocess(text):
-        if not isinstance(text, str):
-            return ''
-
-        tokens = word_tokenize(text.lower())
-        filtered_tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
-        return ' '.join(filtered_tokens)
-
-    df['processed'] = df['document'].apply(preprocess)
-    print("end load")
-    return df
-
-def search_word_func():
-    word = st.session_state['text_input']
-    df = st.session_state['text_df']
-    m = core.make_map_most_popular(word, df['processed'])
-
-    sorted_items = sorted(m.items(), key=lambda x: x[1], reverse=True)    
-
-    core.print_searched_words(sorted_items, 10)
-
-    def create_list(words, size: int):
-        word_list = []
-        count_list = []
-        for i in range(0, size+1):
-            word_list.append(words[i][0])
-            count_list.append(words[i][1])
-        d = {'word':word_list, 'count':count_list}           
-        df = pd.DataFrame(data=d)
-        return df
-    
-    df = create_list(sorted_items, 10)
-    print(df)
-
-    if 'data_frame' in st.session_state:
-        print('is yet!')
-        print(st.session_state['data_frame'])
-    else:
-        print('not yet!')
-
-    st.session_state['data_frame'] = df 
-
-
-text_df = load_data()
+text_df = core.load_data()
 
 if 'text_df' not in st.session_state:
     st.session_state['text_df'] = text_df
@@ -77,5 +13,5 @@ if 'data_frame' not in st.session_state:
     st.session_state['data_frame'] = pd.DataFrame()
 
 st.title('My prilo')
-st.text_input('Search word', key='text_input', on_change=search_word_func)
+st.text_input('Search word', key='text_input', on_change=core.search_word_func)
 st.dataframe(st.session_state['data_frame'])

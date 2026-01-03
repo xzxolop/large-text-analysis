7e4382e3302c3422daffda4d6a33a031a98adb45
Voronin Egor <evoroni2004@gmail.com>
2026-01-03 21:10:15 +0300

    chore: Добавил подсчет времени

--- backend/console-demo/dummy.py
+++ backend/console-demo/dummy.py
@@ -17,6 +17,7 @@ def load_data():
     sentances_df = pd.DataFrame({
         "raw": comments_df["body"]  # Тут создается копия!
     })
+    sentances_df["processed"] = ""
 
     print("sentances_df:\n", sentances_df, "\n")
 
@@ -26,18 +27,13 @@ def load_data():
     stop_words = set(stopwords.words('english'))
     print("stop words:\n", stop_words, "\n")
 
-
-    sentances_df["processed"] = ""
-    print("sentances_df:\n", sentances_df, "\n")
-
     print("sentances list:\n")
     preprocess_df(sentances_df, stop_words)
     print(sentances_df)
 
     # Комментарий -> предложения -> удалить ненужные слова.
 def preprocess_df(sentances_df, stop_words):
-    print("size:", sentances_df["raw"].size)
-    for i in range(sentances_df["raw"].size): #sentances_df["raw"].size
+    for i in range(sentances_df["raw"].size):
         sent = sentances_df["raw"][i]
         sent_list = preprocess_sent(sent, stop_words)
         sentances_df.at[i, "processed"] = sent_list
@@ -50,11 +46,11 @@ def preprocess_sent(text: str, stop_words):
     if not isinstance(text, str):
             return filtered_setances_list
 
-    setances_list = nltk.sent_tokenize(text)
+    setances_list = sent_tokenize(text)
     
     for sent in setances_list:
         filtered_sent = ""
-        words = nltk.word_tokenize(sent)
+        words = word_tokenize(sent)
         for word in words:
             if word not in stop_words:
                 filtered_sent += word
--- backend/console-demo/main.py
+++ backend/console-demo/main.py
@@ -1,3 +1,10 @@
+import time
+
 import dummy
 
+start_time = time.perf_counter()
 dummy.load_data()
+end_time = time.perf_counter()
+
+elapsed_time = end_time - start_time
+print(f"Время выполнения: {elapsed_time:.4f} секунд")

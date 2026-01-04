ef54ee454f0f6d528335673337a9e3a1f313a100
Voronin Egor <evoroni2004@gmail.com>
2026-01-05 00:30:42 +0300

    feat: Добавил минимальный inverted index

---
+++ backend/console-demo/invertedindex.py
@@ -0,0 +1,36 @@
+from nltk.tokenize import word_tokenize
+
+class InvertedIndex:
+    index = dict()
+
+    def __init__(self, sentences: list):
+        for i in range(len(sentences)):
+            sent = sentences[i]
+            words = word_tokenize(sent)
+            for word in words:
+                if word in self.index:
+                    self.index[word].add(i)
+                else:
+                    s = set()
+                    s.add(i)
+                    self.index[word] = s
+        print(self.index)
+    
+    def search(self, search_word):
+        if search_word not in self.index:
+            return {}
+        else:
+            return self.index[search_word]
+    
+sentences = [
+    "Кот сидит на ковре.",
+    "Собака бежит за котом.",
+    "Кот и собака играют.",
+    "Рыжий кот спит на диване.",
+    "Собака охраняет дом."
+]
+
+# Создаем инвертированный индекс
+idx = InvertedIndex(sentences)
+w = idx.search("Кот")
+print("Кот:", w)

2fe7f5210664c38d0e77617a21ae4fb93f3f0a09
Voronin Egor <evoroni2004@gmail.com>
2026-01-05 19:48:36 +0300

    feat: Реализовал последовательный поиск двух слов

--- backend/console-demo/invertedindex.py
+++ backend/console-demo/invertedindex.py
@@ -2,8 +2,13 @@ from nltk.tokenize import word_tokenize
 
 class InvertedIndex:
     index = dict()
+    _sentences: list
+
+    searched_words = set()
+    searched_sentences = set()
 
     def __init__(self, sentences: list):
+        self._sentences = sentences
         for i in range(len(sentences)):
             sent = sentences[i]
             words = word_tokenize(sent)
@@ -15,15 +20,47 @@ class InvertedIndex:
                     s.add(i)
                     self.index[word] = s
         
-    
-    def search(self, search_word):
+    def search(self, search_word) -> set:
         if search_word not in self.index:
             return {}
         else:
             return self.index[search_word]
+        
+    def searchWith(self, search_word) -> set:
+        if search_word in self.searched_words:
+            return {} # TODO: мб кинуть ошибку
+        
+        s = set()
+        if len(self.searched_words) == 0:
+            s = self.search(search_word) # NOTE: это плохо. Возможна потеря данных!
+            self.searched_words.add(search_word)
+            self.searched_sentences = s.copy()
+            return s
+
+        for x in self.searched_sentences:
+            sent = self._sentences[x]
+            words = word_tokenize(sent)
+            if search_word in words:
+                s.add(x)
+
+        self.searched_sentences = s.copy()
+        self.searched_words.add(search_word)
+        return s
     
     def print(self):
         print(self.index)
+
+    def printResult(self):
+        print(self.searched_words, self.searched_sentences)
+
+    def getSentByIndex(self, index: int) -> list:
+        return self._sentences[index].copy()
+
+    def getSentByIndexes(self, indexes: set) -> list:
+        sent_list = []
+        for i in indexes:
+            sent_list.append(self._sentences[i].copy())
+        return sent_list
     
 sentences = [
     "Кот сидит на ковре.",
@@ -37,5 +74,13 @@ sentences = [
 idx = InvertedIndex(sentences)
 idx.print()
 
-w = idx.search("Кот")
-print("Кот:", w)
+#w = idx.search("Кот")
+#print("Кот:", w)
+
+w = idx.searchWith("Кот")
+print("w:", w)
+idx.printResult()
+
+w = idx.searchWith("сидит")
+print("w:", w)
+idx.printResult()

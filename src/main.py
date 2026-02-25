bfd7793431fa644e5b005bae25aae3f8acd60651
Voronin Egor <evoroni2004@gmail.com>
2026-02-25 17:01:54 +0300

    chore: добавлен search_words_sequentially

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from data.data_storage import DataStorage
 from search.search_engine import SearchEngine
 from data.data_exporter import DataExporter
-from demo import tfidf_for_top_words
+import demo
 
 
 data_store = DataStorage()
@@ -10,8 +10,10 @@ sentences = data_store.get_processed_sentences()
 
 engine = SearchEngine(sentences, calc_word_freq=True)
 
-tfidf_for_top_words(engine, top_n=20)
+demo.tfidf_for_top_words(engine, top_n=20)
 
 # Пример использования DataExporter (пока не обязателен для основной логики):
 exporter = DataExporter()
 filepath = exporter.write_mean_tfidf_to_file(engine.get_top_words_with_tfidf(n=20))
+
+demo.search_words_sequentially(engine, ["russia", "china"])
